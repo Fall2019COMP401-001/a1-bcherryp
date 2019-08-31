21ee7782b1a1d6853aeaf47e9f61a4075ce796f1
bcherryp <bcherryp@live.unc.edu>
2019-08-31 14:02:58 -0400

    Math is wrong but it runs, still working on why it is not calculating correctly

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,59 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		//get count of number of inputs
+		int numCustomers = scan.nextInt();
+		
+		//list of objects and values I will need
+		String Fname;
+		String Lname;
+		String Finitial;
+		String itemName;
+		int numItems;
+		int multItems;
+		double price;
+		double sum = 0;
+		double previous = 0;
+		double Ssum;
+		String[] receipt = new String[numCustomers];
+		
+		//for loop to step through scanner inputs
+		for (int i = 0; i < numCustomers; i++) {
+			Fname = scan.next();
+			Finitial = Fname.substring(0, 1) + ".";
+			Lname = scan.next();
+			numItems = scan.nextInt();
+			//String to keep track of the totals
+			String[] total = new String [numItems];
+			//for loop to get sum of items and print
+			for (int n = 0; n < numItems; n++) {
+				multItems = scan.nextInt();
+				itemName = scan.next();
+				price = scan.nextDouble();
+				//calculate sum
+				sum = (multItems * price);
+				
+				Ssum = (previous + sum);
+				
+	
+				previous = sum;
+	
+				
+				String Final = String.format("%.2f", Ssum);
+				
+				//get the total to send to the receipt to print.
+				total[n] = Final;
+				}
+			
+		
+			receipt[i] = Finitial + " " + Lname + " " + total[numItems-1];
+			
+		}
+		for (int i = 0; i < numCustomers; i++) {
+			
+			System.out.println(receipt[i]);
+		
+		}
+		scan.close();
 	}
 }
